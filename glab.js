32f79755bc8fef185be73e6195962c98b5b3b1cc
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-29 11:11:19 -0500

    Add roll class

--- glab.js
+++ glab.js
@@ -1,12 +1,37 @@
 class Character {
   constructor(name) {
-    this.name = name
-    this.health = 100
-    this.inventory = []
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+
+  roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+  }
+}
+
+class Adventurer extends Character {
+  constructor(name, role) {
+    super(name);
+    this.role = role; // Adventurers have specialized roles.
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+
+  // Adventurers have the ability to scout ahead of them.
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
   }
 }
 
 const robin = new Character("Robin");
+robin.roll()
+robin.roll()
+robin.roll()
+robin.roll()
+
 robin.inventory = ["sword", "potion", "artifact"];
 robin.companion = new Character("Leo");
 robin.companion.type = "Cat";
