333fe32e4f0f01594c5478d72f428f1246b2d076
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-29 13:33:29 -0500

    Create Character class

--- solution.js
+++ solution.js
@@ -1,30 +1,42 @@
-
-const adventurer = {
-  name: 'John',
-  health: 10,
-  inventory: ['sword', 'potion', 'axe', 'shield', 'bow'],
-  companion: {
-    name: 'Leo',
-    type: 'Cat',
-    companion: {
-      name: 'Frank',
-      type: 'Flea',
-      inventory: ['small hat', 'sunglasses']
-    }
-  },
-  roll (mod = 0) {
-    const result = Math.floor(Math.random() * 20) + 1 + mod;
-    console.log(`${this.name} rolled a ${result}.`);
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
   }
 }
 
-console.log("Loop through inventory");
-for (let i = 0; i < adventurer.inventory.length; i++) {
-  console.log(adventurer.inventory[i]);
-}
+const rich = new Character('Richard')
+rich.inventory = ['sword', 'potion', 'axe', 'shield']
+rich.companion = new Character('Bella')
+console.log(rich);
 
-console.log("Companion name:", adventurer.companion.name);
-console.log("Companion's companion's second item:", adventurer.companion.companion.inventory[1]);
+// const adventurer = {
+//   name: 'John',
+//   health: 10,
+//   inventory: ['sword', 'potion', 'axe', 'shield', 'bow'],
+//   companion: {
+//     name: 'Leo',
+//     type: 'Cat',
+//     companion: {
+//       name: 'Frank',
+//       type: 'Flea',
+//       inventory: ['small hat', 'sunglasses']
+//     }
+//   },
+//   roll (mod = 0) {
+//     const result = Math.floor(Math.random() * 20) + 1 + mod;
+//     console.log(`${this.name} rolled a ${result}.`);
+//   }
+// }
 
-console.log("Full Object");
-console.log(adventurer);
+// console.log("Loop through inventory");
+// for (let i = 0; i < adventurer.inventory.length; i++) {
+//   console.log(adventurer.inventory[i]);
+// }
+
+// console.log("Companion name:", adventurer.companion.name);
+// console.log("Companion's companion's second item:", adventurer.companion.companion.inventory[1]);
+
+// console.log("Full Object");
+// console.log(adventurer);
