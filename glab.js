facf5465d135626749891945e61dd1aed096f302
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-29 11:08:50 -0500

    Part 2

---
+++ glab.js
@@ -0,0 +1,17 @@
+class Character {
+  constructor(name) {
+    this.name = name
+    this.health = 100
+    this.inventory = []
+  }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+console.log(robin);
