ad5081d70a0f6d2d9efab2148db5184d4f5bb14f
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-29 13:57:52 -0500

    Create Enemy class

--- solution.js
+++ solution.js
@@ -23,44 +23,17 @@ class Hero extends Character {
   }
 }
 
+class Enemy extends Character {
+  constructor(name) {
+    super(name)
+  }
+  attack() {
+    console.log(`${this.name} has launched an attack`);
+  }
+}
+
 const hero = new Hero('Batman')
+const enemy = new Hero('Joker')
 console.log(hero);
+console.log(enemy);
 
-const rich = new Character('Richard')
-// rich.inventory = ['sword', 'potion', 'axe', 'shield']
-rich.addItem('sword')
-rich.addItem('shield')
-rich.addItem('potion')
-
-rich.companion = new Character('Bella')
-console.log(rich);
-
-// const adventurer = {
-//   name: 'John',
-//   health: 10,
-//   inventory: ['sword', 'potion', 'axe', 'shield', 'bow'],
-//   companion: {
-//     name: 'Leo',
-//     type: 'Cat',
-//     companion: {
-//       name: 'Frank',
-//       type: 'Flea',
-//       inventory: ['small hat', 'sunglasses']
-//     }
-//   },
-//   roll (mod = 0) {
-//     const result = Math.floor(Math.random() * 20) + 1 + mod;
-//     console.log(`${this.name} rolled a ${result}.`);
-//   }
-// }
-
-// console.log("Loop through inventory");
-// for (let i = 0; i < adventurer.inventory.length; i++) {
-//   console.log(adventurer.inventory[i]);
-// }
-
-// console.log("Companion name:", adventurer.companion.name);
-// console.log("Companion's companion's second item:", adventurer.companion.companion.inventory[1]);
-
-// console.log("Full Object");
-// console.log(adventurer);
